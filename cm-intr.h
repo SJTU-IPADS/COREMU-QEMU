decefb81f3591c200aebf4e0faa0187afb00a172
Wang Zhaoguo <tigerwang1986@gmail.com>
2010-05-14 16:03:15 -0400

    add coremu related files
    include interrupt timer and thread function body related files, and i386
    architecture specific structure for coremu interrupt mechanism

---
+++ cm-intr.h
@@ -0,0 +1,40 @@
+/*
+ * COREMU Parallel Emulator Framework
+ * Defines qemu related structure and interface for hardware interrupt
+ * 
+ * Copyright (C) 2010 PPI, Fudan Univ. 
+ *  <http://ppi.fudan.edu.cn/system_research_group>
+ *
+ * Authors:
+ *  Zhaoguo Wang	<zgwang@fudan.edu.cn>
+ *  Yufei Chen 		<chenyufei@fudan.edu.cn>
+ *  Ran Liu 		<naruilone@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef CM_INTR_H
+#define CM_INTR_H
+
+/* This structure is used to wrap the interrupt request between different core 
+   thread and hardware thread. Opaque's type is depend on different sources and 
+   target architectures. */
+typedef struct CMIntr {
+    int source;
+    void *opaque;
+} CMIntr;
+
+
+
+
+#endif
---
+++ target-i386/cm-i386-intr.h
@@ -0,0 +1,45 @@
+/*
+ * COREMU Parallel Emulator Framework
+ * Defines qemu related structure and interface for i386 architecture.
+ *
+ * Copyright (C) 2010 PPI, Fudan Univ. 
+ *  <http://ppi.fudan.edu.cn/system_research_group>
+ *
+ * Authors:
+ *  Zhaoguo Wang	<zgwang@fudan.edu.cn>
+ *  Yufei Chen 		<chenyufei@fudan.edu.cn>
+ *  Ran Liu 		<naruilone@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
+ */
+
+ /* Interrupt types for i386 architecture */
+ enum cm_i386_intr_type {
+    I8259_INTR,                 /* Interrupt from i8259 pic */
+    IOAPIC_INTR,                /* Interrupt from IOAPIC */
+    IPI_INTR,                   /* Interrupt from other core */
+    DIRECT_INTR,                /* Direct interrupt (SMI) */
+    SHUTDOWN_REQ,               /* Shut down request */
+ };
+
+/* Interrupt infomation for i8259 pic */
+typedef struct CMPicIntrInfo {
+    int level;                  /* the level of interrupt */
+} CMPicIntrInfo;
+
+/* Interrupt information for IOAPIC */
+typedef struct CMIOAPICIntrInfo {
+    
+} CMIOAPICIntrInfo;
+
